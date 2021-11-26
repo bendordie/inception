f66ae6d1da9003a09e37385ec0a4ebb6a3663f9b
Carmel Shells <cshells@ev-b2.kzn.21-school.ru>
2021-11-26 18:07:24 +0300

    23

--- inception/srcs/mariadb/begin.sh
+++ inception/srcs/mariadb/begin.sh
@@ -1,17 +1,42 @@
-rc default
-/etc/init.d/mariadb setup
 /etc/init.d/mariadb start
 
-mysql -u root <<MYSQL_SCRIPT
+mysql -u root
 CREATE DATABASE wp_db;
 CREATE USER 'cshells'@'%' IDENTIFIED BY '12345';
-GRANT ALL PRIVILEGES ON *.* TO 'cshells';
+GRANT ALL PRIVILEGES ON wp_db.* TO 'cshells';
 FLUSH PRIVILEGES;
-MYSQL_SCRIPT
+mysql exit;
+#MYSQL_SCRIPT
 
 #mysql -u root wp_db < /wp_db.sql
-/etc/init.d/mariadb stop
+#/etc/init.d/mariadb stop
 
-sleep 3
+#sleep 3
 
 /usr/bin/mysqld_safe
+
+
+
+
+
+
+
+
+
+#rc default
+#/etc/init.d/mariadb setup
+#/etc/init.d/mariadb start
+#
+#mysql -u root <<MYSQL_SCRIPT
+#CREATE DATABASE wp_db;
+#CREATE USER 'cshells'@'%' IDENTIFIED BY '12345';
+#GRANT ALL PRIVILEGES ON *.* TO 'cshells';
+#FLUSH PRIVILEGES;
+#MYSQL_SCRIPT
+#
+##mysql -u root wp_db < /wp_db.sql
+#/etc/init.d/mariadb stop
+#
+#sleep 3
+#
+#/usr/bin/mysqld_safe
