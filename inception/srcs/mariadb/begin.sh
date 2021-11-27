ac67ef76583807b283a4cbceb1d8d7635390b8f9
Carmel Shells <cshells@ev-b2.kzn.21-school.ru>
2021-11-27 19:00:29 +0300

    38

--- inception/srcs/mariadb/begin.sh
+++ inception/srcs/mariadb/begin.sh
@@ -2,10 +2,10 @@
 
 sed -i "s/bind-ad/\#bind-ad/" "/etc/mysql/mariadb.conf.d/50-server.cnf"
 sed -i "s/\#port  /port   /" "/etc/mysql/mariadb.conf.d/50-server.cnf"
-if [ ! -d /var/lib/mysql/wordpress/ ]; then
+if [ ! -d /var/lib/mysql/wp_db/ ]; then
 service mysql start
 echo "CREATE DATABASE IF NOT EXISTS wp_db;"| mysql -u root
-echo "CREATE USER IF NOT EXISTS '$cshells'@'%' IDENTIFIED BY '12345';"| mysql -u root
+echo "CREATE USER IF NOT EXISTS 'cshells'@'%' IDENTIFIED BY '12345';"| mysql -u root
 echo "GRANT ALL PRIVILEGES ON wp_db.* TO 'cshells'@'%' WITH GRANT OPTION;"| mysql -u root
 echo "FLUSH PRIVILEGES;"| mysql -u root
 mysqladmin -u root password 12345
