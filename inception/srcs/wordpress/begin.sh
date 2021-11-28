88aefd2c87da053d835c9bd5244972a8477a63ca
Carmel Shells <cshells@ev-b2.kzn.21-school.ru>
2021-11-28 16:09:50 +0300

    66

--- inception/srcs/wordpress/begin.sh
+++ inception/srcs/wordpress/begin.sh
@@ -1,19 +1,21 @@
 #!/bin/sh
 
-if ! $(wp core is-installed --allow-root); then
-	wp config shuffle-salts --allow-root
-    wp core install		\
-        --allow-root    \
-	--path=/var/www/wordpress	\
-        --url=https://cshells.42.fr \
-        --title=CSHELLS_INCEPTION    \
-        --admin_user=cshells     \
-        --admin_password=12345 \
-        --admin_email=cshells@student.21-school.ru
-	wp user create --allow-root frariel frariel@student.21-school.ru --user_pass=12345
-fi
-
 /usr/sbin/php-fpm7 -F -R --nodaemonize
+
+#if ! $(wp core is-installed --allow-root); then
+#	wp config shuffle-salts --allow-root
+#    wp core install		\
+#        --allow-root    \
+#	--path=/var/www/wordpress	\
+#        --url=https://cshells.42.fr \
+#        --title=CSHELLS_INCEPTION    \
+#        --admin_user=cshells     \
+#        --admin_password=12345 \
+#        --admin_email=cshells@student.21-school.ru
+#	wp user create --allow-root frariel frariel@student.21-school.ru --user_pass=12345
+#fi
+
+
 
 #exec "$@"
 
